72e47acb95140e9f5d7d6eb9eabb0d515cd348b9
JAMALEEY <elidrissi.yassir3@gmail.com>
2022-05-09 12:52:33 +0100

    add caseService methods implementation

--- miniPcrud/src/main/java/com/example/minipcrud/service/CaseServiceImpl.java
+++ miniPcrud/src/main/java/com/example/minipcrud/service/CaseServiceImpl.java
@@ -1,4 +1,52 @@
 package com.example.minipcrud.service;
 
-public class CaseServiceImpl {
+import com.example.minipcrud.entity.Case;
+import com.example.minipcrud.repository.CaseRepository;
+import com.example.minipcrud.restControllers.CasesRestControler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+// we add @service annotation to enable service
+@Service
+public class CaseServiceImpl implements CaseService{
+    // set up constructor injection using the caseRepository
+    private CaseRepository caseRepository;
+    @Autowired
+    public CaseServiceImpl(CaseRepository caseRepository){
+        this.caseRepository = caseRepository;
+    }
+
+    @Override
+    public List<Case> findAll() {
+        return caseRepository.findAll();
+    }
+
+    @Override
+    public void saveCase(Case theCase) {
+        caseRepository.save(theCase);
+    }
+
+    // working with Optional (introduced in java8)
+    @Override
+    public Case readOneCase(Long idCase) {
+        Optional<Case> result = caseRepository.
+                findById(idCase);
+        Case theCase = null;
+        if (result.isPresent()) {
+            theCase = result.get();
+        } else {
+            // if we didnt find the case
+            throw new RuntimeException("Cannot find the case - case id : " + idCase );
+
+        }
+        return theCase;
+    }
+
+    @Override
+    public void deleteCase(Long idCase) {
+    caseRepository.deleteById(idCase);
+    }
 }
